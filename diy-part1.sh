063d51b6e72c150397afcaa75c844d7f0fbc4636
hxbbs999 <59958518+hxbbs999@users.noreply.github.com>
2022-12-01 19:19:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# sed -i '1i src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/openwrt-packages/luci-app-unblockneteasemusic
+# git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-packages/luci-app-bypass
+# sed -i '1i src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# sed -i '1i src-git themes https://github.com/Lienol/openwrt-luci' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+# sed -i '1i src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
+# sed -i '1i src-git passwall https://github.com/kiddin9/openwrt-passwall' feeds.conf.default
